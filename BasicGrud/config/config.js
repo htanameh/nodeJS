7803a717ae3634ff4a49fcc69f50af98c9f2d154
Raja <rajasmart48@gmail.com>
2018-06-28 19:59:33 +0530

    Database Configuration and Connection (MongoDB)

---
+++ BasicGrud/config/config.js
@@ -0,0 +1,5 @@
+
+/*jshint esversion: 6 */
+databaseUrl = 'mongodb://localhost:27017/local';
+
+module.exports = databaseUrl ;
--- BasicGrud/index.js
+++ BasicGrud/index.js
@@ -1,14 +1,19 @@
+
+/*jshint esversion: 6 */
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const dbConfig = require('./config/config.js');
+const mongoose = require('mongoose');
 
 // create express app
 const app = express();
 
 // parse requests of URL
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse requests of JSON
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // define a simple route
 app.get('/', (req, res) => {
@@ -18,6 +23,17 @@ app.get('/', (req, res) => {
 //define a different route
 app.get('/',(req,res) => {
 	res.json({"message" : "Welcomr to Test Route!"});
+});
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig)
+.then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...');
+    process.exit();
 });
 
 // listen for requests
