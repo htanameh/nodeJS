6faf5f8b888fcc5a1cb0b9de4754911c50e0e107
Raja <rajasmart48@gmail.com>
2018-06-30 00:26:53 +0530

    Final

--- BasicCRUD/index.js
+++ BasicCRUD/index.js
@@ -5,11 +5,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dbConfig = require('./config/config.js');
 const mongoose = require('mongoose');
-const message = require('./app/controller.js');
 
 
 // create express app
 const app = express();
+
+//Schema of the message
+const modelSchema = mongoose.Schema({
+  id : Number,
+  name : String,
+  msg : String
+}, 
+{
+  timestamp : true
+});
+
+//creating a model 
+var msgModel = mongoose.model("msgModel",modelSchema);
 
 // parse requests of URL
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,33 +44,6 @@ app.get('/index.htm', function (req, res) {
    res.sendFile( __dirname + "/" + "index.htm" );
 });
 
-//route to add Message
-app.get('/process_get', function (req, res) {
-   // Prepare output in JSON format
-   res.json({
-   	"ID" : req.query.id , 
-   	"first_name": req.query.first_name,
-     "message " : req.query.message 
- });
-   message.create;
-});
-
-// Create a new Message
-app.post('/message', message.create);
-
-// Retrieve all Messages
-app.get('/messages', message.findAll);
-
-// Retrieve a single Message with messageId
-app.get('/messages/:messageId', message.findOne);
-
-// Update a Message with messageId
-app.put('/messages/:messageId', message.update);
-
-// Delete a Message with messageId
-app.delete('/messages/:messageId', message.delete);
-
-
 mongoose.Promise = global.Promise;
 
 // Connecting to the database
@@ -68,6 +53,67 @@ mongoose.connect(dbConfig)
 }).catch(err => {
     console.log('Could not connect to the database. Exiting now...');
     process.exit();
+});
+
+//route to add Message
+app.get('/process_get', function (req, res) {
+   var newMsg = new msgModel({
+    id : req.query.id,
+    name : req.query.name,
+    msg : req.query.message
+});
+   newMsg.save((err) => {
+    if(err) console.log("Error in saving message : " + err);
+    else
+      console.log("Message saved Successfully, msg : " + newMsg);
+  });
+   res.json({"message" : newMsg});
+   res.end();
+});
+
+// Retrieve and return all Messages from the database.
+app.get('/get_messages', function (req, res) {
+  msgModel.find().then(msgmodels => {
+    res.json(msgmodels);
+    console.log("Message retrieved Successfully");
+  }).catch(err => {
+    console.log("Error in retrieving message : " + err);
+    res.end();
+  });
+  
+});
+
+//Find a single message with a messageID
+app.get('/get_message/:msgID', function (req, res) {
+  msgModel.find({ id: req.params.msgID }).then(msg => {
+    res.json(msg);
+    console.log("Message retrieved Successfully");
+  }).catch(err => {
+    console.log("Error in retrieving message : " + err);
+    res.end();
+  });
+});
+
+//Find a single message and update
+app.get('/get_message/:msgID/:updateMsg', function (req, res) {
+  msgModel.findOneAndUpdate({ id: req.params.msgID },{msg : req.params.updateMsg }).then(msg => {
+    res.json({"message" : msg});
+    console.log("Message updated Successfully");
+  }).catch(err => {
+    console.log("Error in updating message : " + err);
+    res.end();
+  });
+});
+
+//Delete a single message
+app.get('/delete_message/:msgID', function (req, res) {
+  msgModel.deleteOne({ id: req.params.msgID }).then(msg => {
+    res.json({"message" : msg});
+    console.log("Message deleted Successfully");
+  }).catch(err => {
+    console.log("Error in updating message : " + err);
+    res.end();
+  });
 });
 
 // listen for requests
